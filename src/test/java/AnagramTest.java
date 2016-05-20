85c7f1b5ffe9c0319a40392d86c7121b59d212da
anders.jaensson <anders.jaensson@smartbear.com>
2016-05-20 09:32:14 +0200

    Skeleton for anagram kata

---
+++ src/test/java/AnagramTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Map;
+
+public class AnagramTest {
+    @Test
+    public void test() throws Exception {
+
+    }
+}
